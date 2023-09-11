d21b29fe397ad6cd6150a0fa3070735ae10c66fa
MariusPopa1 <2003popamarius@gmail.com>
2023-09-11 22:31:32 +0300

    first commit

---
+++ src/Exercise1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class  Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Name");
+
+        String name = myObj.nextLine();
+        String shortName =name.charAt(0)+".";
+        for (int i = 0; i < name.length() - 1; i++)
+            if (name.charAt(i) == ' ') {
+                shortName = shortName + Character.toUpperCase(
+                        name.charAt(i + 1));
+                shortName = shortName + "."; }
+        System.out.println(shortName);
+    }
+}
