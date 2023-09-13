e2499c834866d81e52042529c39e999702a70318
MariusPopa1 <2003popamarius@gmail.com>
2023-09-13 16:23:30 +0300

    shorter ver

---
+++ src/Exercise3_short.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Diamond_New {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Size");
+        int size = myObj.nextInt();
+
+        for (int i = -5; i <= size; i = i + 2) {
+            if (i == 1){
+                continue;
+            }
+            int spaces = (java.lang.Math.abs(i)-1) / 2;
+            int symbols = size - spaces*2;
+            for (int j = 0; j < spaces; j++)
+                System.out.print(" ");
+            for (int j = 0; j < symbols; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
+
