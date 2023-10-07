a0fbc4b64873aab058e73d30512dbbdd3fdb6032
MariusPopa1 <2003popamarius@gmail.com>
2023-10-07 19:13:13 +0300

    add colour options

--- src/Lab1/TextColour.java
+++ src/Lab1/TextColour.java
@@ -1,2 +1,24 @@
-package Lab1;public class TextColour {
-}
+package Lab1;
+
+import java.io.*;
+public class TextColour {
+
+// Java Program to Print Colored Text in Console
+
+// Importing input output classes
+
+
+
+        // Declaring ANSI_RESET so that we can reset the color
+        public static final String RESET = "\u001B[0m";
+
+
+        public static final String CYAN = "\u001B[36m";
+
+        public static final String PURPLE = "\u001B[35m";
+        public static final String WHITE = "\u001B[37m";
+        public static final String GREEN = "\u001B[32m";
+        public static final String RED = "\u001B[31m";
+
+    }
+
