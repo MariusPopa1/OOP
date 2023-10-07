e42ebe4e6aaffbf65b352f6d88cef354331d3a38
MariusPopa1 <2003popamarius@gmail.com>
2023-10-07 17:55:19 +0300

    set up faculty class

--- src/Lab1/Enumeration.java
+++
@@ -1,2 +0,0 @@
-package Lab1;public class Enumeration {
-}
--- src/Lab1/Faculty.java
+++ src/Lab1/Faculty.java
@@ -1,5 +1,13 @@
 package Lab1;
 
 public class Faculty {
+private String name;
+private String abbreviation;
+private StudyField studyField;
+public enum StudyField{
+    Mechanical_Engineering,
+    SOFTWARE_ENGINEERING,
+    FOOD_TECHNOLOGY,M
 
+}
 }
