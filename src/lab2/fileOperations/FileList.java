3f051ec193427646d134a407cf4f946078b244c8
MariusPopa1 <2003popamarius@gmail.com>
2023-12-04 04:24:29 +0200

    added a way to store a list of the files present in my specified folder

--- src/lab2/fileOperations/FileList.java
+++ src/lab2/fileOperations/FileList.java
@@ -1,4 +1,21 @@
 package lab2.fileOperations;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FileList {
+    public static List<String> getSnapshotFiles(File folder) {
+        List<String> snapshotFiles = new ArrayList<>();
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                snapshotFiles.add(file.getName());
+            }
+        }
+        return snapshotFiles;
+    }
 }
+// no point in conditions, since my directory is an existing constant, and we're going to use a list
+// to store the file names, since they are unique anyway
