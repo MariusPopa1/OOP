5436625a11a32aed88b18d28c4722e337f8a8cc9
MariusPopa1 <2003popamarius@gmail.com>
2023-12-04 10:15:12 +0200

    changed to hashSet

--- src/lab2/fileOperations/FileList.java
+++ src/lab2/fileOperations/FileList.java
@@ -1,17 +1,18 @@
 package lab2.fileOperations;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileList {
-    public static List<String> getSnapshotFiles(File folder) {
-        List<String> snapshotFiles = new ArrayList<>();
-        File[] files = folder.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                snapshotFiles.add(file.getName());
+    public static Set<String> getSnapshotFiles(File folder) {
+        Set<String> snapshotFiles = new HashSet<>();
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    snapshotFiles.add(file.getName());
+                }
             }
         }
         return snapshotFiles;
