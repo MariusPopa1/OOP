8e652efe7b284a567a720c6e4b9d1c6c9a11da4f
MariusPopa1 <2003popamarius@gmail.com>
2023-12-04 04:01:03 +0200

    added file class

---
+++ src/lab2/FileOperations/FileTask.java
@@ -0,0 +1,6 @@
+package lab2.FileOperations;
+
+public interface FileTask {
+    void execute(String[] args); //execute takes on an array of strings
+}
+// Represents a contract for classes that will perform some actions on files
