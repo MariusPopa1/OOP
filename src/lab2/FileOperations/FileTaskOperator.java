e34884d4f777f791d8bea134eff3071505dfdb0f
MariusPopa1 <2003popamarius@gmail.com>
2023-12-04 04:03:48 +0200

    added an executor

--- src/lab2/FileOperations/FileTaskOperator.java
+++ src/lab2/FileOperations/FileTaskOperator.java
@@ -1,2 +1,13 @@
-package lab2.FileOperations;public class FileTaskOperator {
+package lab2.FileOperations;
+
+public class FileTaskOperator {
+    private FileTask fileTask;
+
+    public FileTaskOperator(FileTask fileAction) {
+        this.fileTask = fileAction;
+    }
+
+    public void execute(String[] args) {
+        fileTask.execute(args);
+    }
 }
