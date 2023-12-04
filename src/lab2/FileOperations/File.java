9bc9a8e59bb4414abf52f261031b345f8b07b200
MariusPopa1 <2003popamarius@gmail.com>
2023-12-04 03:40:20 +0200

    added general files

---
+++ src/lab2/FileOperations/File.java
@@ -0,0 +1,11 @@
+package lab2.FileOperations;
+
+public interface File {
+        int getLineCount();
+        int getWordCount();
+        int getCharacterCount();
+        int getClassCount();
+        int getMethodCount();
+        String getImageDimensions();
+    }
+
